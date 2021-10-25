ca88b2cfefda5e8f0bea9f9a9c03739d62c885b5
michelerenzullo <michele.renzullo98@gmail.com>
2021-10-26 01:51:44 +0200

    typo

--- test/xmp_webconverter_worker.js
+++ test/xmp_webconverter_worker.js
@@ -1,32 +1,32 @@
-onmessage = (e)=> {
-    const files = e.data[0];
-	const args = e.data[1]; 
+onmessage = (e) => {
+	const files = e.data[0];
+	const args = e.data[1];
 	args["input"] = "to_convert/"
 	args["output"] = "converted/"
-	
- /* if (!Object.keys(FS.open('/','r').node.contents).includes(args.input.endsWith("/") ? args.input.slice(0,-1) : args.input))
-	 FS.mkdir(args.input);*/
 
-	 
-	try{FS.mkdir(args.input);}
-	catch{} 
-	
+	/* if (!Object.keys(FS.open('/','r').node.contents).includes(args.input.endsWith("/") ? args.input.slice(0,-1) : args.input))
+		FS.mkdir(args.input);*/
+
+
+	try { FS.mkdir(args.input); }
+	catch { }
+
 	FS.mount(WORKERFS, { files: files }, args.input);
 
-    Module.options(args);
-	
+	Module.options(args);
+
 	FS.unmount(args.input);
 
-	if(files.length==1){
-     postMessage([
-	  Object.keys(FS.open(args.output,'r').node.contents)[0],
-	  new Blob([FS.readFile(args.output + Object.keys(FS.open(args.output,'r').node.contents)[0])], {type: "octet/stream"})
-	 ]);
-	 FS.unlink(args.output + Object.keys(FS.open(args.output,'r').node.contents)[0]);
+	if (files.length == 1) {
+		postMessage([
+			Object.keys(FS.open(args.output, 'r').node.contents)[0],
+			new Blob([FS.readFile(args.output + Object.keys(FS.open(args.output, 'r').node.contents)[0])], { type: "octet/stream" })
+		]);
+		FS.unlink(args.output + Object.keys(FS.open(args.output, 'r').node.contents)[0]);
 	}
 	else {
-	 postMessage(["converted_luts.zip",new Blob([FS.readFile("converted_luts.zip")], {type: "octet/stream"})]);
-	 FS.unlink("converted_luts.zip");
+		postMessage(["converted_luts.zip", new Blob([FS.readFile("converted_luts.zip")], { type: "octet/stream" })]);
+		FS.unlink("converted_luts.zip");
 	}
 
 }
